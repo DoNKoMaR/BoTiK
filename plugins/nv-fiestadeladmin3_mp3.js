4c17545ca362c75e657c7938a73f013faca0f5f0
DoNKoMaR <113529621+DoNKoMaR@users.noreply.github.com>
2022-12-04 15:55:01 +0600

    BoTiK

--- plugins/nv-fiestadeladmin3_mp3.js
+++ plugins/nv-fiestadeladmin3_mp3.js
@@ -3,13 +3,13 @@ import path from 'path'
 
 let handler = async (m, { conn }) => {
 if (!db.data.chats[m.chat].audios && m.isGroup) throw 0
-let vn = './media/Fiesta1.mp3'
+let vn = './media/Far Cry New.mp3'
 conn.sendFile(m.chat, vn, 'Fiesta1.mp3', null, m, true, {
 type: 'audioMessage', 
 ptt: true 
 })
 }
-handler.customPrefix = /fiesta del admin 3|atención grupo|atencion grupo|aviso importante|fiestadeladmin3|fiesta en casa de uriel/i
+handler.customPrefix = /Спамщики|спамщики|Спам|спам|fiestadeladmin3|fiesta en casa de uriel/i
 handler.command = new RegExp
 handler.fail = null
 handler.exp = 100
--- plugins/nv-nyanpasu.js
+++ plugins/nv-nyanpasu.js
@@ -3,13 +3,13 @@ import path from 'path'
 
 let handler = async (m, { conn }) => {
 if (!db.data.chats[m.chat].audios && m.isGroup) throw 0
-let vn = './media/otaku.mp3'
+let vn = './media/Ведьмак 3.mp3'
 conn.sendFile(m.chat, vn, 'otaku.mp3', null, m, true, {
 type: 'audioMessage', 
 ptt: true 
 })
 }
-handler.customPrefix = /Nyapasu|Nyanpasu|nyapasu|Nyapasu|Gambure|Yabure/
+handler.customPrefix = /Пошли в лс|пошли в лс|Лс|лс|В личку|в личку/
 handler.command = new RegExp
 export default handler
 

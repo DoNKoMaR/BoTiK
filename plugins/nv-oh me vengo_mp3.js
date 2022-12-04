5a163de41470675b410e13f116b39cc96c995bc7
DoNKoMaR <113529621+DoNKoMaR@users.noreply.github.com>
2022-12-04 16:11:06 +0600

    BoTiK

--- plugins/nv-oh me vengo_mp3.js
+++ plugins/nv-oh me vengo_mp3.js
@@ -3,13 +3,13 @@ import path from 'path'
 
 let handler = async (m, { conn }) => {
 if (!db.data.chats[m.chat].audios && m.isGroup) throw 0
-let vn = './media/vengo.mp3'
+let vn = './media/Ты мое задание.mp3'
 conn.sendFile(m.chat, vn, 'vengo.mp3', null, m, true, {
 type: 'audioMessage', 
 ptt: true 
 })
 }
-handler.customPrefix = /ho me vengo|oh me vengo|o me vengo|Ho me vengo|Oh me vengo|O me vengo/
+handler.customPrefix = /Задание|задание|Драться будем|драться будем|Раз на раз|раз на раз/
 handler.command = new RegExp
 handler.fail = null
 handler.exp = 100
--- plugins/nv-onichan.js
+++ plugins/nv-onichan.js
@@ -3,13 +3,13 @@ import path from 'path'
 
 let handler = async (m, { conn }) => {
 if (!db.data.chats[m.chat].audios && m.isGroup) throw 0
-let vn = './media/Onichan.mp3'
+let vn = './media/VID-20191225.mp3'
 conn.sendFile(m.chat, vn, 'Onichan.mp3', null, m, true, {
 type: 'audioMessage', 
 ptt: true 
 })
 }
-handler.customPrefix = /oni-chan|onichan|o-onichan/i
+handler.customPrefix = /Где ты|где ты|Ты где|ты где/i
 handler.command = new RegExp
 handler.fail = null
 handler.exp = 100
